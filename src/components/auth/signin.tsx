763164e4fb7125232620a2ee481f5813ac56850f
Antony0101 <vimalt24@gmail.com>
2024-04-09 15:17:47 +0530

    auth page updated

--- src/components/auth/signin.tsx
+++ src/components/auth/signin.tsx
@@ -16,12 +16,12 @@ const SigninComponent = (props:Props) => {
     return (
              <div id="auth-inner-box" class="flex justify-center items-center mt-5">
                 <div class="rounded p-2 border-4 border-slate-950">
-                    <h1 class="text-center text-5xl pt-5">Auth</h1>
-                    <p class="text-center text-3xl pt-10">Welcome to the auth page</p>
-                    <form hx-post="/auth/api/login" hx-target="#auth-inner-box" hx-swap="outerHTML" class="flex flex-col items-center pt-10">
-                        <input name="username" type="text" class="border-2 border-slate-950 p-2 rounded" placeholder="Username" value={props?.values?.username || ''} />
+                    <form hx-post="/auth/api/login" hx-target="#auth-inner-box" hx-swap="outerHTML" class="flex flex-col pt-10">
+                        <label for="username" class="self-start">username</label>
+                        <input name="username" id="username" type="text" class="border-2 border-slate-950 p-2 rounded" placeholder="Username" value={props?.values?.username || ''} />
                         {props?.errors?.username && <p class="text-red-500">{props?.errors?.username}</p>}
-                        <input name="password" type="password" class="border-2 border-slate-950 p-2 rounded mt-5" placeholder="Password" value={props?.values?.password || ''} />
+                        <label for="password" class="self-start mt-5">password</label>
+                        <input name="password" id="password" type="password" class="border-2 border-slate-950 p-2 rounded" placeholder="Password" value={props?.values?.password || ''} />
                         {props?.errors?.password && <p class="text-red-500">{props?.errors?.password}</p>}
                         <button type="submit" class="border-2 border-slate-950 p-2 rounded mt-5">Sign in</button>
                     </form>
--- src/pages/Auth/page.tsx
+++ src/pages/Auth/page.tsx
@@ -3,10 +3,22 @@ import SigninComponent from '../../components/auth/signin.js'
 
 const AuthPage = () =>{
     return (
-        <div>
-            <SigninComponent></SigninComponent>
+        <div >
+            <div class="pt-16 text-center">
+                <h1 class='text-3xl font-extrabold'>Chat App</h1>
+            </div>
+            <div class="flex justify-center pt-20">
+                <h1 class="text-2xl font-bold">Welcome to Chat App</h1>
+            </div>
+            <div class="flex justify-center pt-10">
+                <SigninComponent></SigninComponent>
+            </div>
         </div>
     )
 }
 
 export default AuthPage
+
+// <div>
+// <SigninComponent></SigninComponent>
+// </div>
